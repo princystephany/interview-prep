3b1e914aecf7765a026645b7ccd7203cea490fa0
princystephany <stephanyben6@gmail.com>
2023-05-19 05:33:29 -0700

    3rd commit

--- src/com/string/PalindromeOfStrings.java
+++ src/com/string/PalindromeOfStrings.java
@@ -1,5 +1,25 @@
 package com.string;
 
 public class PalindromeOfStrings {
+public static void main(String[] args) {
+	
+	String  str1 = "malayalam";
+	String  str2 = "malay";
+	
+	boolean flag=true;
+	
+	for(int i=0; i<str2.length();i++) {
+		if(str2.charAt(i)!=str2.charAt(str2.length()-i-1)){
+			flag=false;
+		}
+	}
 
+	if(flag) {
+		System.out.println("It is a palindrome");
+	}
+	else{
+		System.out.println("It is not a palindrome");
+	}
+	
+}
 }
